c73970eea38223255141cf46f580da2be35a6eac
Andre Phosarath <andrepho22@gmail.com>
2024-06-01 09:33:51 -0500

    Completed crud for both book and authors pages - all books, books on sale, authors, favorite authors along with details

--- events/domEvents.js
+++ events/domEvents.js
@@ -1,14 +1,20 @@
-import { deleteBook, getBooks } from '../api/bookData';
+import { deleteBook, getBooks, getSingleBook } from '../api/bookData';
 import { showBooks } from '../pages/books';
 import addBookForm from '../components/forms/addBookForm';
+import viewBook from '../pages/viewBook';
+import { getAuthors, getSingleAuthor } from '../api/authorData';
+import addAuthorForm from '../components/forms/addAuthorForm';
+import { getBookDetails, getAuthorDetails, deleteAuthorBooksRelationship } from '../api/mergedData';
+import { showAuthors } from '../pages/authors';
+import viewAuthor from '../pages/viewAuthor';
 
 const domEvents = () => {
   document.querySelector('#main-container').addEventListener('click', (e) => {
     // TODO: CLICK EVENT FOR DELETING A BOOK
     if (e.target.id.includes('delete-book')) {
       // eslint-disable-next-line no-alert
-      if (window.confirm('Want to delete?')) {
-        console.warn('CLICKED DELETE BOOK', e.target.id);
+      if (window.confirm('Want to delete this gift from the Chicken Lord?')) {
+        // console.warn('CLICKED DELETE BOOK', e.target.id);
         // console.warn(e.target.id.split('--'));
         const [, firebaseKey] = e.target.id.split('--');
 
@@ -26,29 +32,58 @@ const domEvents = () => {
 
     // TODO: CLICK EVENT EDITING/UPDATING A BOOK
     if (e.target.id.includes('edit-book-btn')) {
-      console.warn('EDIT BOOK', e.target.id);
-      console.warn(e.target.id.split('--'));
+      // console.warn('EDIT BOOK', e.target.id);
+      // console.warn(e.target.id.split('--'));
+      const [, firebaseKey] = e.target.id.split('--');
+
+      getSingleBook(firebaseKey).then((bookObj) => addBookForm(bookObj));
+      // getSingleBook(firebaseKey).then(addBookForm); // using the callback method
     }
+
     // TODO: CLICK EVENT FOR VIEW BOOK DETAILS
     if (e.target.id.includes('view-book-btn')) {
-      console.warn('VIEW BOOK', e.target.id);
-      console.warn(e.target.id.split('--'));
+      // console.warn('VIEW BOOK', e.target.id);
+      // console.warn(e.target.id.split('--'));
+      const [, firebaseKey] = e.target.id.split('--');
+
+      getBookDetails(firebaseKey).then(viewBook);
     }
 
     // FIXME: ADD CLICK EVENT FOR DELETING AN AUTHOR
     if (e.target.id.includes('delete-author-btn')) {
       // eslint-disable-next-line no-alert
-      if (window.confirm('Want to delete?')) {
-        console.warn('DELETE AUTHOR', e.target.id);
-        console.warn(e.target.id.split('--'));
+      if (window.confirm('What? Is the Pen mightier than the sword?!')) {
+        // console.warn('DELETE AUTHOR', e.target.id);
+        // console.warn(e.target.id.split('--'));
+        const [, firebaseKey] = e.target.id.split('--');
+        // Call the promise we created on button click to delete all the authors books,
+        // then delete the author
+        deleteAuthorBooksRelationship(firebaseKey).then(() => {
+          getAuthors().then(showAuthors);
+        });
       }
     }
 
     // FIXME: ADD CLICK EVENT FOR SHOWING FORM FOR ADDING AN AUTHOR
     if (e.target.id.includes('add-author-btn')) {
-      console.warn('ADD AUTHOR');
+      // console.warn('ADD AUTHOR');
+      addAuthorForm();
     }
     // FIXME: ADD CLICK EVENT FOR EDITING AN AUTHOR
+    if (e.target.id.includes('update-author')) {
+      // console.warn('EDIT BOOK', e.target.id);
+      // console.warn(e.target.id.split('--'));
+      const [, firebaseKey] = e.target.id.split('--');
+      getSingleAuthor(firebaseKey).then((authorObj) => addAuthorForm(authorObj));
+    }
+
+    // FIXME: ADD CLICK EVENT FOR VIEW AUTHOR DETAILS
+    if (e.target.id.includes('view-author-btn')) {
+      // console.warn('EDIT BOOK', e.target.id);
+      // console.warn(e.target.id.split('--'));
+      const [, firebaseKey] = e.target.id.split('--');
+      getAuthorDetails(firebaseKey).then(viewAuthor);
+    }
   });
 };
 
--- events/formEvents.js
+++ events/formEvents.js
@@ -1,5 +1,9 @@
 import { showBooks } from '../pages/books';
 import { createBook, updateBook, getBooks } from '../api/bookData';
+import {
+  getAuthors, createAuthor, updateAuthor, getFavAuthors
+} from '../api/authorData';
+import { showAuthors } from '../pages/authors';
 
 const formEvents = () => {
   document.querySelector('#main-container').addEventListener('submit', (e) => {
@@ -28,16 +32,54 @@ const formEvents = () => {
     // TODO: CLICK EVENT FOR EDITING A BOOK
     if (e.target.id.includes('update-book')) {
       const [, firebaseKey] = e.target.id.split('--');
-      console.warn('CLICKED UPDATE BOOK', e.target.id);
-      console.warn(firebaseKey);
+      // console.warn('CLICKED UPDATE BOOK', e.target.id);
+      // console.warn(firebaseKey);
+      const payload = {
+        title: document.querySelector('#title').value,
+        description: document.querySelector('#description').value,
+        image: document.querySelector('#image').value,
+        price: document.querySelector('#price').value,
+        author_id: document.querySelector('#author_id').value,
+        sale: document.querySelector('#sale').checked,
+        firebaseKey,
+      };
+      updateBook(payload).then(() => {
+        getBooks().then(showBooks);
+      });
     }
 
     // FIXME: ADD CLICK EVENT FOR SUBMITTING FORM FOR ADDING AN AUTHOR
     if (e.target.id.includes('submit-author')) {
-      console.warn('CLICKED SUBMIT AUTHOR');
+      // console.warn('CLICKED SUBMIT AUTHOR');
+      const payload = {
+        first_name: document.querySelector('#first_name').value,
+        last_name: document.querySelector('#last_name').value,
+        email: document.querySelector('#email').value,
+        favorite: document.querySelector('#favorite').checked,
+      };
+
+      createAuthor(payload).then(({ name }) => {
+        const patchPayload = { firebaseKey: name };
+
+        updateAuthor(patchPayload).then(() => {
+          getAuthors().then(showAuthors);
+        });
+      });
     }
     // FIXME:ADD CLICK EVENT FOR EDITING AN AUTHOR
+    if (e.target.id.includes('update-author')) {
+      // console.warn('CLICKED SUBMIT AUTHOR');
+      const payload = {
+        first_name: document.querySelector('#first_name').value,
+        last_name: document.querySelector('#last_name').value,
+        email: document.querySelector('#email').value,
+        favorite: document.querySelector('#favorite').checked,
+      };
+      updateAuthor(payload).then(() => {
+        getAuthors().then(showAuthors);
+        getFavAuthors();
+      });
+    }
   });
 };
-
 export default formEvents;
